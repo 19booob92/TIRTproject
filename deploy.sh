95026d3d288419266db3fab5a4e6dcd28f349f84
Mateusz Olczak <mateusz.olczak@cohesiva.com>
2015-03-25 12:25:47 +0100

    poprawki

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
+sudo pkill -9 -f jetty
+sudo pkill -9 -f tomcat
+
 mvn clean install -Dmaven.test.skip=true
 cd /home/mateusz/Programy/apache-tomcat-8.0.15/webapps/
 sudo rm -fr *
-mv /home/mateusz/Repos/TIRTproject/target/
+cd /home/mateusz/Repos/TIRTproject/target/
 sudo mv tirt-0.0.1-SNAPSHOT.war ROOT.war
 sudo cp /home/mateusz/Repos/TIRTproject/target/ROOT.war /home/mateusz/Programy/apache-tomcat-8.0.15/webapps/
 
+sudo sh /home/mateusz/Programy/apache-tomcat-8.0.15/bin/startup.sh
+
+
+tail -f /home/mateusz/Programy/apache-tomcat-8.0.15/logs/catalina.out
